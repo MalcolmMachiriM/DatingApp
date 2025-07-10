8740636ca2439c440236e8846bdb991ba29a55f1
Malcolm <machirimalcolm@gmail.com>
2025-07-10 17:41:54 +0200

    --drop off library, --update main pic --delete photos

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -10,12 +10,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context, ITokenService tokenService): BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] // account/regiser
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
-        if(await UserExists(registerDto.Username)) return BadRequest("User Exists");
+        if (await UserExists(registerDto.Username)) return BadRequest("User Exists");
         using var hmac = new HMACSHA512();
         // var user = new AppUser
         // {
@@ -39,27 +39,31 @@ public class AccountController(DataContext context, ITokenService tokenService):
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await context.Users.FirstOrDefaultAsync(x=> x.UserName == loginDto.Username.ToLower());
-        if(user ==null) return Unauthorized("Invalid username");
+        var user = await context.Users
+        .Include(p => p.Photos)
+        .FirstOrDefaultAsync(x =>
+        x.UserName == loginDto.Username.ToLower());
+        if (user == null) return Unauthorized("Invalid username");
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
         var computedHAsh = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
         for (int i = 0; i < computedHAsh.Length; i++)
         {
-            if(computedHAsh[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            if (computedHAsh[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
 
         }
 
         return new UserDto
         {
             Username = user.UserName,
-            Token = tokenService.CreateToken(user)
+            Token = tokenService.CreateToken(user),
+            PhotoUrl = user.Photos?.FirstOrDefault(x => x.IsMain)?.Url
         };
     }
 
     public async Task<bool> UserExists(string username)
     {
-        return await context.Users.AnyAsync(x=>x.UserName.ToLower() == username.ToLower());
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
     }
- }
+}
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -67,8 +67,52 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
 
         if (await userRepository.SaveAllAsync())
             return CreatedAtAction(nameof(GetUser),
-            new { username = user.UserName}, mapper.Map<PhotoDto> (photo));
+            new { username = user.UserName }, mapper.Map<PhotoDto>(photo));
         return BadRequest("Problem adding photo");
+    }
+
+    [HttpPut("set-main-photo/{photoId:int}")]
+    public async Task<ActionResult> SetMainPhoto(int photoId)
+    {
+        var user = await userRepository.GetUserByUserNameAsync(User.GetUsername());
+
+        if (user == null) return BadRequest("could not find user");
+
+        var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+        if (photo == null || photo.IsMain) return BadRequest("Cannot use this as main photo");
+
+        var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+        if (currentMain != null) currentMain.IsMain = false;
+
+        photo.IsMain = true;
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Problem setting main photo");
+    }
+
+    [HttpDelete("delete-photo/{photoId:int}")]
+    public async Task<ActionResult> DeletePhoto(int photoId)
+    {
+        var user = await userRepository.GetUserByUserNameAsync(User.GetUsername());
+
+        if (user == null) return BadRequest("could not find user");
+
+        var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+        if (photo == null || photo.IsMain) return BadRequest("This photo cannot be deleted");
+
+        if (photo.PublicId != null)
+        {
+            var result = await photoservice.DeletePhotoAsync(photo.PublicId);
+            if (result.Error != null) return BadRequest(result.Error.Message);
+        }
+        user.Photos.Remove(photo);
+
+        if (await userRepository.SaveAllAsync()) return Ok();
+
+        return BadRequest("Problem deleting photo");
     }
 
 }
